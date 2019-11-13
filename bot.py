3c29d16b7a3b05975fa7ed1606dbc2edc3b9b701
Blue <55610198+BIuet@users.noreply.github.com>
2019-11-13 08:12:35 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import asyncio
 import os
 
-bot = commands.Bot(command_prefix = '--', case_insensitive=True)
+bot = commands.Bot(command_prefix = 'o ', case_insensitive=True)
 
 cogs = ['cogs.database','cogs.coghandler',]
 
