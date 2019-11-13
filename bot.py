252f94296babd72e0bcbf255ec74de3210d6b074
Blue <55610198+BIuet@users.noreply.github.com>
2019-11-12 17:41:44 -0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+
+bot = commands.Bot(command_prefix = '--', case_insensitive=True)
+
+cogs = ['cogs.database',]
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} - {bot.user.id}')
+    for cog in cogs:
+        bot.load_extension(cog)
+    print(bot.cogs)
+    await bot.change_presence(activity=discord.Game(name='type --help for commamds!', type=0))
+    return
+    
+bot.run('BOT TOKEN', bot=True, reconnect=True)
