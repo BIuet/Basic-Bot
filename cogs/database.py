645faf3a8dbc0ec7bb1a1c6e58f42e2b9b2bc281
Blue <55610198+BIuet@users.noreply.github.com>
2019-11-13 08:13:58 -0800

    Update database.py

--- cogs/database.py
+++ cogs/database.py
@@ -21,7 +21,7 @@ class Database(commands.Cog):
     async def on_guild_join(guild):
         connect()
         c.execute(f"CREATE TABLE IF NOT EXISTS users(user STR, tank STR, score INT)")
-        await guild.owner.send(f'Heya! Just to let you know, Photon Bot has been added to your discord server, {guild}!\nFeel free to type ``--help``')
+        await guild.owner.send(f'Heya! Just to let you know, Photon Bot has been added to your discord server, {guild}!\nFeel free to type ``o help`` at any time to receive owner exclusive commands!')
         
     @commands.command(
         name='ping',
