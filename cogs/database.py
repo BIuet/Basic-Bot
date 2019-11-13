7eb0015947be665d0c9b1daf8c6211f985349305
Blue <55610198+BIuet@users.noreply.github.com>
2019-11-12 18:35:39 -0800

    Update database.py

--- cogs/database.py
+++ cogs/database.py
@@ -1,1 +1,34 @@
+import discord
+from discord.ext import commands
+from datetime import datetime as d
+import os
+import asyncio
+import sqlite3
 
+class Database(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+        
+    def connect():
+      conn = sqlite3.connect("arras.db")
+      c = conn.cursor()
+      
+    def close():
+      conn.close()
+        
+    @commands.command(
+        name='ping',
+        description='The ping command',
+        aliases=['p']
+    )
+    async def ping_command(self, ctx):
+        start = d.timestamp(d.now())
+
+        msg = await ctx.send(content='Pinging')
+
+        await msg.edit(content=f'Pong!\nOne message round-trip took {( d.timestamp( d.now() ) - start ) * 1000 }ms.')
+        return
+        
+def setup(bot):
+    bot.add_cog(Database(bot))
