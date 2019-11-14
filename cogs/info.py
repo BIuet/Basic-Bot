b00dca3a955b0e6030d6ae130653663c85f38168
Blue <55610198+BIuet@users.noreply.github.com>
2019-11-13 22:23:53 -0800

    Create info.py

---
+++ cogs/info.py
@@ -0,0 +1,117 @@
+import discord
+from discord.ext import commands
+import datetime
+import asyncio
+
+class Info(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(aliases=["ri","role"], no_pm=True)
+    @commands.guild_only()
+    async def roleinfo(self, ctx, *, role: discord.Role):
+        '''Shows information about a role'''
+        guild = ctx.guild
+
+        since_created = (ctx.message.created_at - role.created_at).days
+        role_created = role.created_at.strftime("%d %b %Y %H:%M")
+        created_on = "{} ({} days ago!)".format(role_created, since_created)
+        members = ''
+        i = 0
+        for user in role.members:
+            members += f'{user.name}, '
+            i+=1
+            if i > 30:
+                break
+
+        if str(role.colour) == "#000000":
+            colour = "default"
+            color = ("#%06x" % random.randint(0, 0xFFFFFF))
+            color = int(colour[1:], 16)
+        else:
+            colour = str(role.colour).upper()
+            color = role.colour
+
+        em = discord.Embed(colour=color)
+        em.set_author(name=role.name)
+        em.add_field(name="Users", value=len(role.members))
+        em.add_field(name="Mentionable", value=role.mentionable)
+        em.add_field(name="Hoist", value=role.hoist)
+        em.add_field(name="Position", value=role.position)
+        em.add_field(name="Managed", value=role.managed)
+        em.add_field(name="Colour", value=colour)
+        em.add_field(name='Creation Date', value=created_on)
+        em.add_field(name='Members', value=members[:-2], inline=False)
+        em.set_footer(text=f'Role ID: {role.id}')
+
+        await ctx.send(embed=em)
+        
+    @commands.command(aliases=['server','info'], no_pm=True)
+    @commands.guild_only()
+    async def serverinfo(self, ctx, server_id : int=None):
+        '''See information about the server.'''
+        server = self.bot.get_server(id=server_id) or ctx.guild
+        total_users = len(server.members)
+        online = len([m for m in server.members if m.status != discord.Status.offline])
+        text_channels = len([x for x in server.channels if isinstance(x, discord.TextChannel)])
+        voice_channels = len([x for x in server.channels if isinstance(x, discord.VoiceChannel)])
+        categories = len(server.channels) - text_channels - voice_channels
+        passed = (ctx.message.created_at - server.created_at).days
+        created_at = "Since {}. That's over {} days ago!".format(server.created_at.strftime("%d %b %Y %H:%M"), passed)
+
+        colour = await ctx.get_dominant_color(server.icon_url)
+
+        data = discord.Embed(description=created_at,colour=colour)
+        data.add_field(name="Region", value=str(server.region))
+        data.add_field(name="Users", value="{}/{}".format(online, total_users))
+        data.add_field(name="Text Channels", value=text_channels)
+        data.add_field(name="Voice Channels", value=voice_channels)
+        data.add_field(name="Categories", value=categories)
+        data.add_field(name="Roles", value=len(server.roles))
+        data.add_field(name="Owner", value=str(server.owner))
+        data.set_footer(text="Server ID: " + str(server.id))
+        data.set_author(name=server.name, icon_url=None or server.icon_url)
+        data.set_thumbnail(url=None or server.icon_url)
+        try:
+            await ctx.send(embed=data)
+        except discord.HTTPException:
+            em_list = await embedtobox.etb(data)
+            for page in em_list:
+                await ctx.send(page)
+                
+    @commands.command(
+        name='ping',
+        description='The ping command',
+        aliases=['p']
+    )
+    async def ping_command(self, ctx):
+        start = d.timestamp(d.now())
+
+        msg = await ctx.send(content='Pinging')
+
+        await msg.edit(content=f'Pong!\nOne message round-trip took {( d.timestamp( d.now() ) - start ) * 1000 }ms.')
+        return
+                
+    def has_access():
+        def predicate(ctx):
+            return ctx.message.author.id == 625354389061894145 or ctx.message.author == ctx.message.guild.owner
+        return commands.check(predicate)
+        
+    @commands.command(aliases=['del','p','wipe','purge','clear'])
+    @has_access()
+    async def purge(self, ctx, limit : int, member:discord.Member=None):
+        '''Clean a number of messages'''
+        if member is None:
+            await ctx.purge(limit=limit+1)
+        else:
+            async for message in ctx.channel.history(limit=limit+1):
+                if message.author is member:
+                    await message.delete()
+        
+    @commands.Cog.listener()
+    async def on_guild_join(guild):
+        await guild.owner.send(f'Heya! Just to let you know, Photon Bot has been added to your discord server, {guild}!\nFeel free to type ``o help`` at any time to receive a commands help message!')
+        
+def setup(bot):
+    bot.add_cog(Info(bot))
