7923b0b00b8aee829c2f2ca9950e3d5d7b152fd5
Blue <55610198+BIuet@users.noreply.github.com>
2019-11-20 07:07:06 -0800

    Update search.py

--- cogs/search.py
+++ cogs/search.py
@@ -24,26 +24,23 @@ class Search(commands.Cog):
                     except Exception:
                         await ctx.send("Oh no! The cats are on fire!")
                         
-    @commands.command(aliases=["urban", "ud"])
+    @commands.command(aliases=['ud'])
     async def urban(self, ctx, *, arg):
         search = arg.replace(" ","+")
         url = "http://api.urbandictionary.com/v0/define?term={}".format(search)
-        try:
-            async with aiohttp.ClientSession() as cs:
-                async with cs.get(url) as r:
-                    result = await r.json(content_type= None)
-                    if result["list"]:
-                        definition = result['list'][0]['definition']
-                        example = result['list'][0]['example']
-                        defs = len(result['list'])
-                        embed = discord.Embed(title='Top Definition', description=definition, colour=embedColor(self))
-                        embed.set_author(name=arg, icon_url='https://i.imgur.com/bLf4CYz.png')
-                        embed.add_field(name="Example:", value=example, inline=False)
-                        await ctx.send(embed=embed)
-                    else:
-                        await ctx.send("Your search terms gave no results.")
-        except:
-            await ctx.send("Error.")
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(url) as r:
+                result = await r.json(content_type= None)
+                if result["list"]:
+                    definition = result['list'][0]['definition']
+                    example = result['list'][0]['example']
+                    defs = len(result['list'])
+                    embed = discord.Embed(title='Top Definition', description=definition)
+                    embed.set_author(name=arg, icon_url='https://i.imgur.com/bLf4CYz.png')
+                    embed.add_field(name="Example:", value=example, inline=False)
+                    await ctx.send(embed=embed)
+                else:
+                    await ctx.send("Your search terms gave no results.")
         
 def setup(bot):
     bot.add_cog(Search(bot))
